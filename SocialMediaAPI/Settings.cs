8fff0e6c8d623ccb8f8517ef2a2cefd21287e197
PattareeyaNu <pattareeya@double-p.co.th>
2021-11-03 16:31:05 +0700

    Fix app id

--- SocialMediaAPI/Settings.cs
+++ SocialMediaAPI/Settings.cs
@@ -8,7 +8,7 @@ namespace SocialMediaAPI
     public class Settings
     {
         public static string facebookserviceBase = "https://facebook.com/v10.0/";
-        public static string FacebookAppId = "1226370894513966";
-        public static string FacebookAppSecret = "39cfd5a8eca3645f460939fd2118de9c";
+        public static string FacebookAppId = "423535735989844";
+        public static string FacebookAppSecret = "85922d67675c0f0fdbfabe3d367406e0";
     }
 }
