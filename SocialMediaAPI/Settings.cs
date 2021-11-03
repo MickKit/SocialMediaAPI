6c62b7044272339d8c398113e0bb3f8088db0b9e
PattareeyaNu <pattareeya@double-p.co.th>
2021-11-03 15:18:41 +0700

    Edit FB App ID

--- SocialMediaAPI/Settings.cs
+++ SocialMediaAPI/Settings.cs
@@ -8,7 +8,7 @@ namespace SocialMediaAPI
     public class Settings
     {
         public static string facebookserviceBase = "https://facebook.com/v10.0/";
-        public static string FacebookAppId = "473687073735741";
-        public static string FacebookAppSecret = "13c95ae44d7670454f720f23ec433369";
+        public static string FacebookAppId = "1226370894513966";
+        public static string FacebookAppSecret = "39cfd5a8eca3645f460939fd2118de9c";
     }
 }
