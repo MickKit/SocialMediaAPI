cf545386d5f25ac1b886a655aea6655b940c462c
PattareeyaNu <pattareeya@double-p.co.th>
2021-11-04 10:54:56 +0700

    Add lib component and edit drawing control

--- SocialMediaAPI/App_Start/BundleConfig.cs
+++ SocialMediaAPI/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace SocialMediaAPI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/FontAwesome/css/all.min.css"));
         }
     }
 }
